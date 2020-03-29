c076786c414c2bae16aa7874bd44a355add5534a
Ravina Kaur Gill <ravina@Ravinas-MacBook-Pro.local>
2020-03-28 21:50:11 -0700

    Changed backgroud image and added further styling

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   <div className="App">
   <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div className="container">
-      <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+      {/* <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a> */}
       <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i className="fa fa-bars"></i>
@@ -64,8 +64,8 @@ function App() {
   <header className="masthead">
     <div className="container">
       <div className="intro-text">
-        <div className="intro-lead-in">Welcome To Our Studio!</div>
-        <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
+        {/* <div className="intro-lead-in">Harpreet Thandi</div> */}
+        <div className="intro-heading text-uppercase">Pro-Touch Tiling</div>
         <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
       </div>
     </div>
