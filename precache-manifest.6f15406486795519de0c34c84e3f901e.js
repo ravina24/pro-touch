af2c7532ec2d64639f0f92366021eeb20a268379
Ravina Kaur Gill <ravina@Ravinas-MacBook-Pro.local>
2020-03-31 00:27:28 -0700

    Updates

---
+++ precache-manifest.6f15406486795519de0c34c84e3f901e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a2e44448b4322f3a9dfc0cc7c9a30f",
+    "url": "/pro-touch/index.html"
+  },
+  {
+    "revision": "63d411dae6207664beaf",
+    "url": "/pro-touch/static/css/main.193ae6cf.chunk.css"
+  },
+  {
+    "revision": "37bf1c78849562365cbe",
+    "url": "/pro-touch/static/js/2.180697ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pro-touch/static/js/2.180697ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d411dae6207664beaf",
+    "url": "/pro-touch/static/js/main.afb4dfb7.chunk.js"
+  },
+  {
+    "revision": "23dbb680cd54d3f150db",
+    "url": "/pro-touch/static/js/runtime-main.f22a8bb6.js"
+  },
+  {
+    "revision": "785d13c2c7550f0a321cff84dd125899",
+    "url": "/pro-touch/static/media/backsplash.785d13c2.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pro-touch/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/pro-touch/static/media/map-image.f40ed5b1.png"
+  }
+]);
